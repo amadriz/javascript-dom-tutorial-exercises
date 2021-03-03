47d2731434891bcf1bc8f1824ecf999b0043552a
Allan Madriz <ahovenga@gmail.com>
2021-03-03 00:01:35 +0000

    JS DOM excercices

--- .breathecode/resets/05-Create-DOM-Element-First/index.js
+++ .breathecode/resets/05-Create-DOM-Element-First/index.js
@@ -4,6 +4,3 @@ document.querySelector("#myDiv").appendChild(elem);
 
 //Your code here
 // Don'change the code above
-let text = document.createElement("p");
-text.innerHTML = "Hello World";
-document.querySelector("#text").appendChild(text).style.background = "yellow";
--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	let elem = document.createElement("div");
+	elem.innerHTML = "Hello World";
 
-
+	document.body.appendChild(elem).style.background = "yellow";
 });
