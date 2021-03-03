1d6f8a4eba63b16ff2b918da2b9398118308fc98
Allan Madriz <ahovenga@gmail.com>
2021-03-03 00:52:39 +0000

    JS DOM excercices 2

--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
+	let list = document.createElement("li");
+	let text = document.createTextNode("Forth Element");
 
+	list.appendChild(text);
+	document.getElementById("myList").appendChild(text);
 });
--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,21 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+	let allCountries = [];
+	// recorre el arreglo
+	for (let i = 0; i < countries.length; i++) {
+		// Crear la el tag o elemento option
+		let node = document.createElement("option");
+		// Crear atributo del input value y agregarle items del array
+		node.setAttribute("value", countries[i]);
+		let textNode = document.createTextNode(countries[i]);
+		node.appendChild(textNode);
+		allCountries = document.getElementById("mySelect").appendChild(node);
+	}
+	return allCountries;
 };
+
+function displayAlert(country) {
+	alert(country);
+}
